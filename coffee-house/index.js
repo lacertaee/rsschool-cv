bec6205fb2b7d5dd9c10e8e0c4087919b0c39112
lacertaee <105096407+lacertaee@users.noreply.github.com>
2023-12-09 15:49:03 +0400

    feat: add main and menu pages

---
+++ coffee-house/index.js
@@ -0,0 +1,80 @@
+const slider = document.querySelector(".slider-ul");
+const rightBtn = document.querySelector(".button-right");
+const leftBtn = document.querySelector(".button-left");
+const slides = Array.from(slider.children);
+const track = document.querySelector(".controls");
+const controls = Array.from(track.children);
+const menuBtn = document.querySelector(".menu-btn");
+const menuLink = document.querySelector(".mn-link");
+const backToHome = document.querySelector(".back-to-home");
+
+console.log(controls);
+
+// Move right
+
+document.addEventListener("DOMContentLoaded", () => {
+    rightBtn.addEventListener("click", () => {
+        const current = document.querySelector('.current');
+        const next = current.nextElementSibling;
+        const move = next.style.left;
+
+        slider.style.transform = `translateX(-${move})`;
+        console.log(move);
+        current.classList.remove('current');
+        next.classList.add("current");
+
+        
+        controls[slides.indexOf(current)].classList.remove('current-btn');
+        controls[slides.indexOf(next)].classList.add('current-btn');
+    })
+})
+
+// Move left
+
+document.addEventListener("DOMContentLoaded", () => {
+    leftBtn.addEventListener("click", () => {
+        const current = document.querySelector('.current');
+        const prev = current.previousElementSibling;
+        const move = prev.style.left;
+
+        console.log(move);
+        if (current === slides[2]) {
+            slider.style.transform = `translateX(-${move})`;
+        } else if (current === slides[1]) {
+            slider.style.transform = `translateX(0rem)`;
+        }
+
+        current.classList.remove('current');
+        prev.classList.add("current");
+
+
+        controls[slides.indexOf(current)].classList.remove('current-btn');
+        controls[slides.indexOf(prev)].classList.add('current-btn');
+    })
+
+    // Menu page
+    function hide() {
+        document.querySelector('main').classList.add('hide');
+        document.querySelector('.about').classList.add('hide');
+        document.querySelector('.mobile').classList.add('hide');
+        document.querySelector('.hero').classList.add('hide');
+
+        l = ['main', '.about', '.mobile', '.hero'];
+
+        l.forEach(element => {
+            document.querySelector(`${element}`).addEventListener('transitionend', () => {
+                document.querySelector(`${element}`).style.display = 'none';
+            });
+        });
+        document.querySelector('.show').style.display ='block';
+        void document.querySelector('.show').offsetWidth;
+        document.querySelector('.show').style.opacity = '1';
+
+    }
+
+
+    menuLink.addEventListener('click', hide);
+    menuBtn.addEventListener('click', hide);
+    
+    
+})
